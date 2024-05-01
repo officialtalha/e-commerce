cff11a8cda4dbcfbeeda4b650e4eeacb0d42b68e
officialtalha <alam.shane272@.com>
2024-05-01 18:10:34 +0530

    fix

--- public/checkout.js
+++ public/checkout.js
@@ -1,3 +1,31 @@
+const userLogin = document.getElementById('user-login');
+const ifLoggedIn = localStorage.getItem('user');
+let token;
+//checking whether user is loggen in or not 
+if (ifLoggedIn) {
+    let info = JSON.parse(ifLoggedIn);
+    token = info.token;
+    userLogin.textContent = `👤${info.name}`;
+}
+//user login nav bar
+userLogin.addEventListener('click', (e) => {
+    try {
+        e.preventDefault();
+        if (ifLoggedIn) {
+            const flag = confirm('Do you want to logout.');
+            if (flag) {
+                localStorage.removeItem('user');
+                window.location.href = './login.html';
+            }
+        } else {
+            window.location.href = './login.html';
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+});
+
 const info = localStorage.getItem('checkout');
 const total = JSON.parse(info);
 document.getElementById('subtotal').textContent = `₹ ${total}`;
